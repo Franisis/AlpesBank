5ef41a451659edd771526978e0ea593df2532701
Gabriela Garcia <g.garcias2@uniandes.edu.co>
2024-03-15 18:33:34 -0500

    name

--- AlpesBank/AlpesBank/settings.py
+++ AlpesBank/AlpesBank/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'AlpesBank.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': str(BASE_DIR / 'db.sqlite3'),
+        'NAME': 'tarjetas_db',
         'USER':'sin_miedo_al_exito',
         'PASSWORD':'SinMiedoAlExito',
         'HOST':'10.24.240.3',
