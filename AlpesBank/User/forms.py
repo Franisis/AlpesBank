78f5f0dcca13eccd0e38657ca45230b5ddd2b43d
Franisis <f.guzmana@uniandes.edu.co>
2024-03-14 21:49:08 -0500

    fix form post

--- AlpesBank/User/forms.py
+++ AlpesBank/User/forms.py
@@ -2,12 +2,13 @@ from django import forms
 from .models import User
 
 class UserForm(forms.ModelForm):
-    model = User
-    fields = [
-        
-        'nombre',
-        'cedula',
-        'correo',
-        'telefono',
+    class Meta:
+        model = User
+        fields = [
 
-        ]
+            'nombre',
+            'cedula',
+            'correo',
+            'telefono',
+
+            ]
