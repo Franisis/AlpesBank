bc9e63dd12c5a061d75d8cc8c08837b53b3f1ab8
Franisis <f.guzmana@uniandes.edu.co>
2024-03-14 21:00:59 -0500

    Post user

--- AlpesBank/User/urls.py
+++ AlpesBank/User/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns =[
     path('users/', views.userGet, name = 'userGet'),
+    path('userCreate/', views.userPost, name = 'userPost'),
 ]
--- AlpesBank/User/views.py
+++ AlpesBank/User/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from .logic import logic_user as ul
 from django.core import serializers
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .forms import UserForm
 
 
 
@@ -12,4 +14,11 @@ def userGet(request):
         users = ul.get_users()
         users_dto = serializers.serialize('json', users)
         return HttpResponse(users_dto, 'application/json')
+
+def userPost(request):
+    if request.method=="POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('userCreate'))
 
