f26e1f6e8fc61906a0b6f4b45416fab0a5ac5cf9
Franisis <f.guzmana@uniandes.edu.co>
2024-05-08 20:17:23 -0500

    ultimo intento :(

--- AlpesBank/AlpesBank/views.py
+++ AlpesBank/AlpesBank/views.py
@@ -12,14 +12,14 @@ def registro_usuario(request):
     if request.method == 'POST':
         form = UserForm(request.POST)
         form.save()
-        nombre = request.POST.get('nombre')
-        respuesta = requests.get('http://35.188.169.4:8080/usercrm/')
-        mensaje = respuesta.json()['mensaje']
+        #nombre = request.POST.get('nombre')
+        #respuesta = requests.get('http://35.188.169.4:8080/usercrm/')
+        #mensaje = respuesta.json()['mensaje']
         #return JsonResponse({'mensaje': mensaje})
-        if mensaje == '1':
-            return render(request, 'registro_exitoso.html')
-        else:
-            return render(request, 'registro_fallido.html')
+        #if mensaje == '1':
+         #   return render(request, 'registro_exitoso.html')
+        #else:
+         #   return render(request, 'registro_fallido.html')
     else:
         return render(request, 'registro_usuario.html')
 
