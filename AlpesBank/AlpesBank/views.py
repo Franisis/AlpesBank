a27c5d82c3aa3bab68cd8edc2db0f18d301dfc02
Franisis <f.guzmana@uniandes.edu.co>
2024-05-08 19:45:35 -0500

    templates y views

--- AlpesBank/AlpesBank/views.py
+++ AlpesBank/AlpesBank/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 import requests
 from AlpesBank.auth0backend import getRole
+from User.forms import UserForm
 
 
 def index(request):
@@ -22,9 +23,13 @@ def registro_usuario(request):
 
 def formulario_cliente(request):
     if request.method == 'POST':
+        formulario = UserForm(request.POST)
+        if formulario.is_valid():
+            formulario.save()
         # Procesar el formulario si es necesario
-        return render(request, 'formulario_cliente.html')
-
+            return render(request, 'formulario_cliente.html')
+    else:
+        render(request, 'formulario_cliente.html')
     if request.method == 'GET':
         respuesta = requests.get('http://35.188.169.4:8080/usercrm/')
         mensaje = respuesta.json()['mensaje']
