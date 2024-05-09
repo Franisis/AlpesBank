762d9fcae90f94516e409f21e11f6f2451b6b2d4
Franisis <f.guzmana@uniandes.edu.co>
2024-05-08 20:33:08 -0500

    SQL logic

--- AlpesBank/User/logic/logic_user.py
+++ AlpesBank/User/logic/logic_user.py
@@ -1,7 +1,7 @@
 from ..models import User
 
 def get_users():
-    users = User.objects.raw("SELECT * FROM users")
+    users = User.objects.raw("SELECT * FROM user")
     return users
 
 def create_user(form):
