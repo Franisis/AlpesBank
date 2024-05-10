15f45c4f1b6b55b3145db781efdb262431959bfd
Francois Morales <f.moraless@uniandes.edu.co>
2024-05-10 18:05:48 -0500

    INTENTo5

--- AlpesBank/User/logic/logic_user.py
+++ AlpesBank/User/logic/logic_user.py
@@ -9,8 +9,13 @@ def create_user(form):
     user.save()
     return ()
 
+# def get_by_id(user_pk):
+#     users = User.objects.raw("SELECT * FROM User WHERE name = %s", [user_pk])
+#     user = next(iter(users), None)
+#     return user
+
 def get_by_id(user_pk):
-    users = User.objects.raw("SELECT * FROM User WHERE name = %s", [user_pk])
+    users = User.objects.raw("SELECT * FROM User")
     user = next(iter(users), None)
     return user
 
