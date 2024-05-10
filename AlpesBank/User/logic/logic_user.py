b64e33ba3a47839acf2c31ae662dddaf22e57aba
Francois Morales <f.moraless@uniandes.edu.co>
2024-05-10 18:39:34 -0500

    Agregar ORM Django

--- AlpesBank/User/logic/logic_user.py
+++ AlpesBank/User/logic/logic_user.py
@@ -1,28 +1,19 @@
 from ..models import User
 from django.db import connection
 
-def get_users():
-    with connection.cursor() as cursor:
-        cursor.execute("SELECT * FROM User")
-        return cursor.fetchall()
 
-# def get_users():
-#     users = User.objects.all()
-#     return users
+def get_users():
+    users = User.objects.all()
+    return users
 
 def create_user(form):
     user = form.save()
     user.save()
     return ()
 
-# def get_by_id(user_pk):
-#     users = User.objects.raw("SELECT * FROM User WHERE name = %s", [user_pk])
-#     user = next(iter(users), None)
-#     return user
-
 def get_by_id(user_pk):
-    users = User.objects.raw("SELECT * FROM User")
-    user = next(iter(users), None)
+    user = User.objects.get(pk=user_pk)
     return user
+
 
 
