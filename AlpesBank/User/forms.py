172ecaf7b47f87454911baa4a72a1654278931e4
Gabriela Garcia <g.garcias2@uniandes.edu.co>
2024-05-10 22:53:49 -0500

    f

--- AlpesBank/User/forms.py
+++ AlpesBank/User/forms.py
@@ -1,23 +1,17 @@
 from django import forms
-from django.core.validators import EmailValidator, RegexValidator, MaxLengthValidator, MinLengthValidator
 from .models import User
+from django.core.validators import EmailValidator, RegexValidator, MaxLengthValidator
 
 class UserForm(forms.ModelForm):
     name = forms.CharField(
         max_length=100, 
-        validators=[MinLengthValidator(1), MaxLengthValidator(100)],
-        error_messages={
-            'max_length': 'El nombre debe tener máximo 100 caracteres.',
-            'min_length': 'El nombre es un campo obligatorio.'
-        }
+        validators=[MaxLengthValidator(100)],
+        error_messages={'max_length': 'El nombre debe tener máximo 100 caracteres.'}
     )
     lastName = forms.CharField(
         max_length=100, 
-        validators=[MinLengthValidator(1), MaxLengthValidator(100)],
-        error_messages={
-            'max_length': 'El apellido debe tener máximo 100 caracteres.',
-            'min_length': 'El apellido es un campo obligatorio.'
-        }
+        validators=[MaxLengthValidator(100)],
+        error_messages={'max_length': 'El apellido debe tener máximo 100 caracteres.'}
     )
     correo = forms.EmailField(
         validators=[EmailValidator()],
@@ -33,8 +27,8 @@ class UserForm(forms.ModelForm):
         error_messages={'invalid': 'Por favor ingresa un número de teléfono válido de Colombia.'}
     )
     cedula = forms.CharField(
-        validators=[RegexValidator(regex=r'^\d{5,10}$', message="La cédula debe tener entre 5 y 10 dígitos.")],
-        error_messages={'invalid': 'La cédula solo debe contener números y tener entre 5 y 10 dígitos.'}
+        validators=[RegexValidator(regex=r'^\d+$', message="La cédula solo debe contener números.")],
+        error_messages={'invalid': 'La cédula solo debe contener números.'}
     )
 
     class Meta:
