25bcadc5e597f0b7c585698383195e2d34cf7a92
Francois Morales <f.moraless@uniandes.edu.co>
2024-05-28 19:54:22 -0500

    Implementacin Bd Documental

--- AlpesBank/User/logic/logic_user.py
+++ AlpesBank/User/logic/logic_user.py
@@ -1,19 +1,75 @@
 from ..models import User
 from django.db import connection
-
+from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 def get_users():
-    users = User.objects.all()
+    client = MongoClient("mongodb://monitoring_user:isis2503@10.128.0.70:27017")
+    db = client.monitoring_db
+    db = db.find({})
+    users = [ User.from_mongo(user) for user in db ]
+    client.close()
+
     return users
 
-def create_user(form):
-    user = form.save()
-    user.save()
-    return ()
+def get_by_id(id):
+    client = MongoClient("mongodb://monitoring_user:isis2503@10.128.0.70:27017")
+    db = client.monitoring_db
+    variables_collection = db['users']
+    user = variables_collection.find_one({'id': id})
+    client.close()
 
-def get_by_id(user_pk):
-    user = User.objects.get(pk=user_pk)
+    if user is None:
+        raise ValueError('Variable not found')
+
+    return User.from_mongo(user)
+
+def verifyUserData(data):
+    
+    user = User()
+    user.id = data['id']
+    user.name = data['name']
+    user.lastname = data['lastname']
+    user.cedula = data['cedula']
+    user.correo = data['correo']
+    user.telefono = data['telefono']
+
     return user
+
+def createUser(data):
+
+    # Verify variable data
+    user = verifyUserData(data)
+
+    # Create variable in MongoDB
+    client = MongoClient("mongodb://monitoring_user:isis2503@10.128.0.70:27017")
+    db = client.monitoring_db
+    variables_collection = db['variables']
+    user.id = variables_collection.insert(
+        {
+            'id': user.id,
+            'correo': user.correo,
+            'name': user.name,
+            'lastname': user.lastname,
+            'cedula': user.cedula,
+            'telefono': user.telefono
+        }
+    )
+    client.close()
+    return user
+
+# def get_users():
+#     users = User.objects.all()
+#     return users
+
+# def create_user(form):
+#     user = form.save()
+#     user.save()
+#     return ()
+
+# def get_by_id(user_pk):
+#     user = User.objects.get(pk=user_pk)
+#     return user
 
 
 
